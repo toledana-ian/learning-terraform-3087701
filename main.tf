8430f3fd56a390e05076786920b92ff699ffc27e
Christian Toledana <toledana.ian@gmail.com>
2024-01-08 04:08:27 +0800

    Add AWS VPC, subnet, and security group resources to main.tf
    New resources for AWS VPC, subnet, and security group have been added into the Terraform configuration in main.tf. The AWS instance resource has also been updated to utilize the new subnet and security group.

--- main.tf
+++ main.tf
@@ -14,9 +14,26 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+resource "aws_vpc" "web_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "web_subnet" {
+  vpc_id     = aws_vpc.web_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "web_security_group" {
+  vpc_id = aws_vpc.web_vpc.id
+}
+
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+  subnet_id     = aws_subnet.web_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_security_group.id]
+
 
   tags = {
     Name = "HelloWorld"
