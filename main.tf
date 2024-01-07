129edaa50f5b75b4a17551ce22f5e125a0cba7fd
Christian Toledana <toledana.ian@gmail.com>
2024-01-08 07:09:40 +0800

    Simplify Terraform configuration and improve security settings
    This commit includes the removal of the dotenv Terraform provider and an update in the AWS provider, which no longer retrieves credentials from AWS Secrets Manager. It also refactors the security group resources in main.tf for better readability and simplicity.

--- main.tf
+++ main.tf
@@ -25,51 +25,19 @@ module "blog_sg" {
   name        = "web-server"
   description = "Security group for web-server with HTTP ports open within VPC"
 
-  vpc_id = data.aws_vpc.default.id
-  ingress_rules = ["http-80-tcp", "http-80-tcp"]
-}
+  vpc_id              = data.aws_vpc.default.id
 
-resource "aws_security_group" "blog_security_group" {
-  name        = "blog-security-group"
-  description = "Allow http and https in. Allow everything out."
+  ingress_rules       = ["http-80-tcp", "http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog_security_group.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog_security_group.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog_security_group.id
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog_security_group.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
